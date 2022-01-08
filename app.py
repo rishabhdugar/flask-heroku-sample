04ab05e4fecc2a451bbeaf6773be46379a028a08
Rishabh <rishabh.dugar.com@gmail.com>
2022-01-08 12:55:05 +0530

    added shopify

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import shopify
+import binascii
+import os
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -40,6 +43,26 @@ def userNew():
   db.session.commit()
   return redirect(url_for('index'))
 
+@app.route('/accessToken', methods=['POST'])
+def accessToken():
+  API_KEY = '0072294b270c3a24e9a9dfda9bf0fc31'
+  API_SECRET = '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'
+  shopify.Session.setup(api_key=API_KEY, secret=API_SECRET)
+
+  shop_url = "9xworks.myshopify.com"
+  api_version = '2020-10'
+  access_token = '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'
+  session = shopify.Session(shop_url, api_version, access_token)
+  shopify.ShopifyResource.activate_session(session)
+
+  shop = shopify.Shop.current() # Get the current shop
+  #product = shopify.Product.find(179761209) # Get a specific product
+  print(shop)
+  # execute a graphQL call
+  return shopify.GraphQL().execute("{ shop { name id } }")
+
+
+  print(res)
 if __name__ == '__main__':
   db.create_all()
   port = int(os.environ.get('PORT', 5000))
